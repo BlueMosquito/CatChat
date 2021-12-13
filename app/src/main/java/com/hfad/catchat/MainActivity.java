710adc231f62241aebbc6edfba56fd584584b627
Hamee <61717325+BlueMosquito@users.noreply.github.com>
2021-12-13 11:32:40 +0100

    last time

--- app/src/main/java/com/hfad/catchat/MainActivity.java
+++ app/src/main/java/com/hfad/catchat/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hfad.catchat;
 
-import android.content.Intent;
+import
+        android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
